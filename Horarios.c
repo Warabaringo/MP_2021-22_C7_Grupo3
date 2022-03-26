69ebe75ea6cd897da979d0a60fe3b2b117ec630f
Warabaringo <cristian.heredia.bravo@gmail.com>
2022-03-26 23:57:36 +0100

    Horarios 1.0

--- Horarios.c
+++ Horarios.c
@@ -1,23 +1,155 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "Horarios.h"
+#include "horarios.h"
+#include "alumnos.h"
+
 
 int main() {
+	unsigned n;
+	horario *horarios = leer_horarios(&n);
+	
+	free(horarios);
+	
+	return 0;
+}
+
+void menu_admin_horarios(horario *horarios, unsigned nHorarios) {
+	int op, encontrado;
+	char id[4];
+	
+	puts("Introduzca la operacion deseada: agregar horas de clase(1), eliminar horas(2), modificar horas(3), listar horarios(4), salir(0)");
+	printf("%i", &op);
+	do{
+		switch(op){
+			case 1:
+				puts("Introduce id de profesor");
+				scanf("%s", &id);
+				if((econtrado = encontrar_profesor(horarios, id, nHorarios)) == -1)
+					puts("Profesor no encontrado");
+				else {
+					agregar_horas(&horarios[encontrado]);
+				}
+				break;
+			case 2:
+				puts("Introduce id de profesor");
+				scanf("%s", &id);
+				if((econtrado = encontrar_profesor(horarios, id, nHorarios)) == -1)
+					puts("Profesor no encontrado");
+				else {
+					eliminar_horas(&horarios[encontrado]);
+				}
+				break;
+			case 3:
+				puts("Introduce id de profesor");
+				scanf("%s", &id);
+				if((econtrado = encontrar_profesor(horarios, id, nHorarios)) == -1)
+					puts("Profesor no encontrado");
+				else {
+					modificar_horas(&horarios[encontrado]);
+				}
+				break;
+			case 4:
+				mostrar_horarios(horarios, nHorarios);
+				break;
+		}
+	}while(salir_menu() == 0);
+}
+
+int encontrar_profesor(horario *horarios, char *idProfesor, unsigned nHorarios) {
+	int i = 0, encontrado = -1;
+	
+	while(encontrado == -1 && i < nHorarios) {
+		if(strcmp(horarios[i].id_profesor, idProfesor) == 0)
+			encontrado = i;
+		i++;
+	}
+	
+	return encontrado;
+}
+
+void agregar_horas(horario *hor) {
+	unsigned horas;
+	
+	puts("Introduzca el numero de horas para agregar");
+	scanf("%u", &horas);
+	
+	if((horas+hor->hora_clase) > 6)
+		puts("El numero de horas introducido excede el limite permitido, es mayor a 6");
+	else
+		hor->hora_clase = hor->hora_clase + horas;
+	
+}
+
+void eliminar_horas(horario *hor) {
+	unsigned horas;
+	
+	puts("Introduzca el numero de horas para eliminar");
+	scanf("%u", &horas);
+	
+	if((hor->hora_clase - horas) < 1)
+		puts("El numero de horas introducido excede el limite permitido, es menor a 1");
+	else
+		hor->hora_clase = hor->hora_clase - horas;
 
 }
 
-horario *leer_horario(unsigned *nProfesor){
+void modificar_horas(horarios *hor) {
+	unsigned horas;
+	
+	puts("Introduzca el numero de horas nuevo");
+	scanf("%u", &horas);
+	
+	if(horas < 1 || horas > 6)
+		puts("El numero de horas excede el limite permitido");
+	else
+		hor->hora_clase = horas;
+
+}
+
+void mostrar_horarios(horario *horarios, unsigned nHorarios) {
+	int i;
+	
+	for(i = 0; i < nHorarios; i++)
+		mostrar_horario(&horario[i]);
+		
+}
+
+void mostrar_horario(const horario *hor) {
+	printf("Id Profesor: %s\n", hor->id_profesor);
+	printf("Dia de clase: %i\n", hor->dia_clase);
+	printf("Hora de clase: %i\n", hor->hora_clase);
+	printf("Id materia: %s\n", hor->id_materia);
+	printf("Grupo: %s\n", hor->grupo);
+}
+
+horario *leer_horarios(unsigned *nHorarios){
     horario *horarios = NULL;
     horario hor;
 
     FILE *f = fopen("Horarios.txt", "r");
-    char linea[127], *token;
+    char linea[21], *token;
     unsigned n = 0;
 
-    while(fgets(linea, 127, f) != NULL) {
-        token = strok(linea, "-");
+    while(fgets(linea, 21, f) != NULL) {
+        token = strtok(linea, "-");
         strcpy(hor.id_profesor, token);
-        token = strtok
+        token = strtok(NULL, "-");
+        hor.dia_clase = atoi(token);
+        token = strtok(NULL, "-");
+        hor.hora_clase = atoi(token);
+        token = strtok(NULL, "-");
+        strcpy(hor.id_materia, token);
+        token = strtok(NULL, "\n");
+        strcpy(hor.grupo, token);
+
+
+        n++;
+        horarios = (horario*) realloc(horarios, n *sizeof(horarios));
+        horarios[n-1] = hor;
     }
+    fclose(f);
+    *nHorarios = n;
+    
+    return horarios;
 }
--- Horarios.h
+++ Horarios.h
@@ -9,4 +9,19 @@ typedef struct {
     char grupo[11];
 } horario;
 
+void menu_admin_horarios(horario *horarios, unsigned nHorarios);
+
+int encontrar_profesor(horario *horarios, char *idProfesor, unsigned nHorarios);
+
+void agregar_horas(horario *hor);
+
+void eliminar_horas(horario *hor);
+
+void modificar_horas(horarios *hor);
+
+void mostrar_horarios(horario *horarios, unsigned nHorarios);
+
+horario *leer_horarios(unsigned *nHorarios);
+
+void mostrar_horario(const horario *hor);
 #endif /* HORARIOS_H */
